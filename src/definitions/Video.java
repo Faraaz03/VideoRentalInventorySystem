b78d216910bad576087bcdda6bd7cde45e605ef8
faraaz0 <faraaz.siddiqui_cs19@gla.ac.in>
2020-08-17 13:34:09 +0530

    add Video.java

---
+++ src/definitions/Video.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Divyansh Bhardwaj (dbc2201)
+ *   Date: 17-08-2020
+ *   Time: 13:31
+ *   File: Video.java
+ */
+
+package definitions;
+
+public class Video {
+}
+
